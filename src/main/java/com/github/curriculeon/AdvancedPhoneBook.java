93bf924e7a90feeadf5ddf70efde492f84e1aaa5
George Kamau <alphons3k@yahoo.com>
2020-01-07 10:21:06 -0500

    final

--- src/main/java/com/github/curriculeon/AdvancedPhoneBook.java
+++ src/main/java/com/github/curriculeon/AdvancedPhoneBook.java
@@ -1,5 +1,7 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,21 +9,22 @@ import java.util.Map;
  * Created by leon on 1/23/18.
  */
 public class AdvancedPhoneBook {
+    HashMap<String, String> phoneBook = new HashMap<String, String>();
+    List<String> a = new ArrayList();
+
     public AdvancedPhoneBook(Map<String, List<String>> map) {
+
     }
 
     public AdvancedPhoneBook() {
         this(null);
     }
 
-    public void add(String name, String phoneNumber) {
-    }
+    public void add(String name, String phoneNumber) { phoneBook.put(name, phoneNumber); }
 
-    public void addAll(String name, String... phoneNumbers) {
-    }
+    public void addAll(String name, String... phoneNumbers) { }
 
-    public void remove(String name) {
-    }
+    public void remove(String name) { }
 
     public Boolean hasEntry(String name) {
         return null;
--- src/main/java/com/github/curriculeon/SimplePhoneBook.java
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -1,32 +1,55 @@
 package com.github.curriculeon;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 12/16/2019.
  */
 public class SimplePhoneBook {
-    public void add(String name, String phoneNumberToAdd) {
+    ArrayList<String> simplePhoneBook = new ArrayList<>();
+    // HashMap<String, String> phoneBook = new HashMap<>();
+    LinkedHashMap<String, String> phoneBook = new LinkedHashMap<>();
 
+    public void add(String name, String phoneNumberToAdd) {
+        phoneBook.put(name, phoneNumberToAdd);
     }
 
     public String get(String name) {
-        return null;
+        return phoneBook.get(name);
     }
 
     public Boolean hasEntry(String phoneNumber) {
-        return null;
+        return phoneBook.containsValue(phoneNumber);
     }
 
     public void remove(String name) {
+        phoneBook.remove(name);
+    }
 
+    public String lookup(String name){
+        String answer = null;
+        if (phoneBook.containsValue(name)){
+            answer = phoneBook.get(name);
+        }
+        return answer;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        String ans[];
+        ArrayList<String> arr = new ArrayList<>();
+        for(String key: phoneBook.keySet()) {
+            arr.add(key);
+        }
+        return arr;
     }
 
     public String reverseLookup(String phoneNumber) {
-        return null;
+        String ans = null;
+        for(String key: phoneBook.keySet()) {
+            if(phoneBook.get(key).equals(phoneNumber)) {
+                ans = (key);
+            }
+        }
+        return ans;
     }
 }
